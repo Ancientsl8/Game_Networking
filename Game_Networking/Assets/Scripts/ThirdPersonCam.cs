3ad76ab4b0aa2e85ecd05379da29749c656cc9ba
Ancientsl8 <61736186+Ancientsl8@users.noreply.github.com>
2023-10-25 01:37:40 +0800

    Added CameraMovement

--- Game_Networking/Assets/Scripts/ThirdPersonCam.cs
+++ Game_Networking/Assets/Scripts/ThirdPersonCam.cs
@@ -4,31 +4,19 @@ using UnityEngine;
 
 public class ThirdPersonCam : MonoBehaviour
 {
-    [SerializeField] private Transform orientation;
-    [SerializeField] private Transform player;
-    [SerializeField] private Transform playerObj;
-    [SerializeField] private Rigidbody rb;
+    //Camera Variables here
+    Vector3 _look;
+    [SerializeField] float rotateSpeed;
 
-    [SerializeField] float rotationSpeed;
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, player.position.z);
-        orientation.forward = viewDir.normalized;
+        //Get mouse movement
+        _look.y = Input.GetAxis("Mouse X");
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        if (inputDir != Vector3.zero)
-        {
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
-        }
+        transform.eulerAngles += new Vector3(0, _look.y * rotateSpeed * Time.deltaTime, 0);
     }
 }
